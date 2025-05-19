6419ec1374456e337b7bc4fb2d12d3114504c0f5
JostinVT <jostin_viter@hotmail.com>
2025-05-18 21:15:55 -0500

    Se agrego Cursos

--- src/main/resources/db/migration/V2__create_table_curso.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE cursos (
-  id INT NOT NULL AUTO_INCREMENT,
-  nombre VARCHAR(100) NOT NULL,
-  descripcion VARCHAR(255),
-  creditos INT NOT NULL,
-  enabled BIT(1) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE (nombre)
-);
